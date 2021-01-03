231a11089851400646f3ec23bc5b8b6d077c4a37
Ravi Kumar <ravikumarsinghsolanki@gmail.com>
2021-01-03 22:50:18 +0530

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace XamlDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public void ApplySkin(Uri skinDictionaryUri)
+        {
+            
+            ResourceDictionary skinDict = Application.LoadComponent(skinDictionaryUri) as ResourceDictionary;
+
+            Collection<ResourceDictionary> mergedDicts = base.Resources.MergedDictionaries;
+
+           
+            if (mergedDicts.Count > 0)
+                mergedDicts.Clear();
+
+           
+            mergedDicts.Add(skinDict);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace XamlDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string CustomText
+        {
+            get { return (string)GetValue(CustomTextProperty); }
+            set { SetValue(CustomTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CustomText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CustomTextProperty =
+            DependencyProperty.Register("CustomText", typeof(string), typeof(TextBlock), new PropertyMetadata("Item 1",new PropertyChangedCallback(changed_custom_text)),
+                new ValidateValueCallback(validate_custom_text)
+                
+                );
+
+        private static bool validate_custom_text(object value)
+        {
+            //TODO
+            return true;
+        }
+
+        private static void changed_custom_text(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            string oldval = e.OldValue.ToString();
+            string newval = e.NewValue.ToString();
+            MessageBox.Show("Value changed");
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void addbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (itemlist.Items.Contains(itemnametxt.Text))
+            {
+                MessageBox.Show("Please choose a different Task name.","Duplicate Task",MessageBoxButton.OK,MessageBoxImage.Error);
+                return;
+            }
+            itemlist.Items.Add(itemtxtbox.Text);
+        }
+
+        private void itemlist_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (completeditemlist.Items.Contains(itemlist.SelectedItem))
+                return;
+
+            
+            completeditemlist.Items.Add(itemlist.SelectedItem +" - on"+ DateTime.Now.ToString());
+            itemlist.Items.Remove(itemlist.SelectedItem);
+        }
+        private void ContextMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = e.OriginalSource as MenuItem;
+            string skinDictPath = item.Tag as string;
+            Uri skinDictUri = new Uri(skinDictPath, UriKind.Relative);
+            App app = Application.Current as App;
+
+            app.ApplySkin(skinDictUri);
+        }
+
+       
+    }
+}
